258f95bd5e98b2146b1b12e4415038648e0530b2
mokshsinghji <moksh@choudry.org>
2024-04-14 11:35:50 +0100

    Revert "feat: added react-query"
    This reverts commit b968d8b94f308dbc6437b073220ef443a03bd96a.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,48 @@
+import { useEffect } from "react";
 import "./App.css";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-
-import HomePage from "./components/pages/HomePage";
-
-const queryClient = new QueryClient();
+import { ReminderInsert } from "./types/Reminder";
+import { DataTable } from "./components/remindersTable/data-table";
+import { remindersState } from "./state/remindersState";
+import { useAtom } from "jotai";
 
 function App() {
+  const [reminders, setReminders] = useAtom(remindersState);
+
+  // fill with test data
+  useEffect(() => {
+    const testReminders: ReminderInsert[] = [
+      {
+        title: "Test 1",
+        description: "This is test 1",
+        completed: false,
+        datetime: Date.now(),
+        reminder: 0
+      },
+      {
+        title: "Test 2",
+        description: "This is test 2",
+        completed: false,
+        datetime: Date.now(),
+        reminder: 0
+      },
+      {
+        title: "Test 3",
+        description: "This is test 3",
+        completed: false,
+        datetime: Date.now(),
+        reminder: 0
+      }
+    ];
+
+    setReminders(testReminders);
+  }, []) // TODO: FILL FROM LOCALSTORAGE OR RUST
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <HomePage />
-    </QueryClientProvider>
-  )
+    <div className="p-2">
+      <h1 className="text-center text-2xl">Reminders</h1>
+      <DataTable data={reminders} />
+    </div>
+  );
 }
 
 export default App;
